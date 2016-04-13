faf9a91f5526dd289973e1b2ffcc93d4a4289c20
Marc Pina <mpina@solomo365.com>
2016-04-13 16:58:48 +0200

    Removing message filter.

--- src/mod_irc_connection.erl
+++ src/mod_irc_connection.erl
@@ -1353,7 +1353,7 @@ process_kick(StateData, Chan, From, Nick, String) ->
 							 attrs =
 							     [{<<"code">>, <<"307">>},
                                                               {<<"moderator">>, From},
-                                                              {<<"message">>, filter_message(Msg)}],
+                                                              {<<"message">>, Msg}],
 							 children = []}]}]}).
 
 process_nick(StateData, From, NewNick) ->
